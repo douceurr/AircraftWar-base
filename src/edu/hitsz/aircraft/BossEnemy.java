40fcc919adff716ba1df73dde11a078613b0336c
duskphantom <187173118@qq.com>
2022-03-20 13:22:46 +0800

    add classes

---
+++ src/edu/hitsz/aircraft/BossEnemy.java
@@ -0,0 +1,12 @@
+package edu.hitsz.aircraft;
+
+import edu.hitsz.application.Game;
+import edu.hitsz.application.Main;
+import edu.hitsz.bullet.AbstractBullet;
+import edu.hitsz.prop.InterfaceProp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BossEnemy extends AbstractAircraft {
+}
--- src/edu/hitsz/aircraft/EliteEnemy.java
+++ src/edu/hitsz/aircraft/EliteEnemy.java
@@ -1,21 +1,13 @@
 package edu.hitsz.aircraft;
 
-import edu.hitsz.application.Game;
 import edu.hitsz.application.Main;
 import edu.hitsz.bullet.AbstractBullet;
 
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 普通敌机
- * 不可射击
- *
- * @author hitsz
- */
-public class MobEnemy extends AbstractAircraft {
-
-    public MobEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
+public class EliteEnemy extends AbstractAircraft{
+    public EliteEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
         super(locationX, locationY, speedX, speedY, hp);
     }
 
